55be6b385e05ca7eecc0c1d47c73650059296370
AJ120C <Anthonyjordan2003@gmail.com>
2024-06-13 08:21:15 -0500

    UwU Cafe

---
+++ uwucafe/app.js
@@ -0,0 +1,407 @@
+
+Categories = {
+    1: "Combos",
+    2: "Mains",
+    3: "Sides",
+    4: "Deserts",
+    5: "Drinks",
+    6: "Business Deals"
+}
+
+Menus = [
+    {
+        "name": "Lucky Bento Box Combo",
+        "cost": 500,
+        "category": 1,
+        "image": "LuckyBentoBoxCombo.png"
+    },
+    {
+        "name": "9 Lives Combo",
+        "cost": 300,
+        "category": 1,
+        "image": "9LivesCombo.png"
+    },
+    {
+        "name": "Kitty Cat Meal",
+        "cost": 275,
+        "category": 1,
+        "image": "KittyCatMeal.png"
+    },
+    {
+        "name": "Cat Burglar Combo",
+        "cost": 260,
+        "category": 1,
+        "image": "CatBurglarCombo.png"
+    },
+    {
+        "name": "Pawcakes Combo",
+        "cost": 185,
+        "category": 1,
+        "image": "PawcakesCombo.png"
+    },
+    {
+        "name": "Fourth of July Meal",
+        "cost": 175,
+        "category": 1,
+        "image": "FourthofJulyMeal.png"
+    },
+    {
+        "name": "Bandit Combo",
+        "cost": 150,
+        "category": 1,
+        "image": "BanditCombo.png"
+    },
+    {
+        "name": "Om Nom Omurice",
+        "cost": 165,
+        "category": 2,
+        "image": "OmNomOmurice.png"
+    },
+    {
+        "name": "Sugio Cat Su Sando",
+        "cost": 160,
+        "category": 2,
+        "image": "SugioCatSuSando.png"
+    },
+    {
+        "name": "Kira Kira Curry",
+        "cost": 135,
+        "category": 2,
+        "image": "KiraKiraCurry.png"
+    },
+    {
+        "name": "Hamburg Steak",
+        "cost": 155,
+        "category": 2,
+        "image": "HamburgSteak.png"
+    },
+    {
+        "name": "Doki Doki Pancakes",
+        "cost": 100,
+        "category": 2,
+        "image": "DokiDokiPancakes.png"
+    },
+    {
+        "name": "Kitty Shish kabobs",
+        "cost": 80,
+        "category": 2,
+        "image": "KittyShishkabobs.png"
+    },
+    {
+        "name": "California Sushi Roll Platter",
+        "cost": 200,
+        "category": 2,
+        "image": "CaliforniaSushiRollPlatter.png"
+    },
+    {
+        "name": "Tempura Dragon Sushi Roll Platter",
+        "cost": 200,
+        "category": 2,
+        "image": "TempuraDragonSushiRollPlatter.png"
+    },
+    {
+        "name": "Spicy Girl Sushi Roll Platter",
+        "cost": 200,
+        "category": 2,
+        "image": "SpicyGirlSushiRollPlatter.png"
+    },
+    {
+        "name": "Chicken noodle Soup",
+        "cost": 100,
+        "category": 3,
+        "image": "ChickennoodleSoup.png"
+    },
+    {
+        "name": "Kitty Rice Balls",
+        "cost": 45,
+        "category": 3,
+        "image": "KittyRiceBalls.png"
+    },
+    {
+        "name": "Rice bowl",
+        "cost": 40,
+        "category": 3,
+        "image": "Ricebowl.png"
+    },
+    {
+        "name": "House Salad",
+        "cost": 15,
+        "category": 3,
+        "image": "HouseSalad.png"
+    },
+    {
+        "name": "Korean Corn Dogs",
+        "cost": 100,
+        "category": 3,
+        "image": "KoreanCornDogs.png"
+    },
+    {
+        "name": "Ballaberry Cupcake",
+        "cost": 50,
+        "category": 4,
+        "image": "BallaberryCupcake.png"
+    },
+    {
+        "name": "Dragon Fire Cupcake",
+        "cost": 50,
+        "category": 4,
+        "image": "DragonFireCupcake.png"
+    },
+    {
+        "name": "Meowchi Mochi Ice Cream",
+        "cost": 30,
+        "category": 4,
+        "image": "MeowchiMochiIceCream.png"
+    },
+    {
+        "name": "Ginger Kitty Cookie",
+        "cost": 30,
+        "category": 4,
+        "image": "GingerKittyCookie.png"
+    },
+    {
+        "name": "Purrfect Parfait",
+        "cost": 40,
+        "category": 4,
+        "image": "PurrfectParfait.png"
+    },
+    {
+        "name": "Oxygen Cake",
+        "cost": 200,
+        "category": 4,
+        "image": "OxygenCake.png"
+    },
+    {
+        "name": "Strawberry Shortcake",
+        "cost": 200,
+        "category": 4,
+        "image": "StrawberryShortcake.png"
+    },
+    {
+        "name": "Firework Cookies",
+        "cost": 30,
+        "category": 4,
+        "image": "FireworkCookies.png"
+    },
+    {
+        "name": "UwU Bomb Pop",
+        "cost": 15,
+        "category": 4,
+        "image": "UwUBombPop.png"
+    },
+    {
+        "name": "Boba Tea",
+        "cost": 60,
+        "category": 5,
+        "image": "BobaTea.png"
+    },
+    {
+        "name": "Lovely Hot Chocolate",
+        "cost": 55,
+        "category": 5,
+        "image": "LovelyHotChocolate.png"
+    },
+    {
+        "name": "Matcha Coffee",
+        "cost": 55,
+        "category": 5,
+        "image": "MatchaCoffee.png"
+    },
+    {
+        "name": "Sweet Herbal Tea",
+        "cost": 50,
+        "category": 5,
+        "image": "SweetHerbalTea.png"
+    },
+    {
+        "name": "Pink lemonade",
+        "cost": 40,
+        "category": 5,
+        "image": "Pinklemonade.png"
+    },
+    {
+        "name": "Bubble Tea",
+        "cost": 150,
+        "category": 5,
+        "image": "BubbleTea.png"
+    },
+    {
+        "name": "Meowcaroons 1X2",
+        "cost": 2,
+        "category": 6,
+        "image": "Meowcaroons1X2.png"
+    },
+    {
+        "name": "Meowcaroons 4x6",
+        "cost": 6,
+        "category": 6,
+        "image": "Meowcaroons4x6.png"
+    },
+    {
+        "name": "Meowcaroons 8x12",
+        "cost": 12,
+        "category": 6,
+        "image": "Meowcaroons8x12.png"
+    },
+    {
+        "name": "Meowcaroons 12X20",
+        "cost": 20,
+        "category": 6,
+        "image": "Meowcaroons12X20.png"
+    }
+]
+
+
+
+
+var discount = 30;
+var mainContent = $(".main-content");
+var orderContent = $(".order-content");
+var contentHTML = '';
+function createMenu() {
+    for (key in Categories) {
+        var contentCategory = `<div class="content-category">
+                                <h1>${Categories[key]}</h1>
+                                <div class="content-category-items">
+                                
+                            `;
+        for (menu of Menus) {
+            if (menu.category == key) {
+                var encodedName = menu.name.replace(/\s+/g, '-');
+                var categoryItem = `
+                    <div class="category-item" data-name = ${encodedName} data-cost = ${menu.cost} data-image = ${menu.image}>
+                        <img src=assets/${menu.image} alt="">
+                        <div class="info-text">
+                            <h2>${menu.name}</h2>
+                            <h2><strong>${menu.cost}$</strong></h2>
+                        </div>
+                        <div class="add-to-basket-btn">
+                            <p>Add To Basket</p>
+                        </div>
+                    </div>
+            `;
+                contentCategory += categoryItem; // Öğeyi kategoriye ekler
+                //console.log(menu.name, menu.category);
+            }
+        }
+
+        contentCategory += '</div>'; // Kategoriyi kapatır
+        contentCategory += '</div>'; // Kategoriyi kapatır
+        //console.log(contentCategory);
+        //contentHTML += contentCategory; // Kategori içeriğini dizeye ekler
+        mainContent.append(contentCategory);
+    }
+}
+
+$(document).ready(function () {
+    createMenu();
+    //console.log(contentHTML);
+    //mainContent.append(contentHTML);
+});
+
+$(document).on('click', '.add-to-basket-btn', function () {
+    var name = $(this).parent().data('name');
+    var cost = $(this).parent().data('cost');
+    var image = $(this).parent().data('image');
+    //console.log(name, cost, image);
+    if ( orderContent.find(`#${name}`).length > 0) {
+        
+        orderContent.find(`#${name} > .add-container > .count`).text(parseInt(orderContent.find(`#${name} > .add-container > .count`).text()) + 1);
+        orderContent.find(`#${name} > .item-info > h2 > strong`).text(parseInt(orderContent.find(`#${name} > .item-info > h2 > strong`).text()) + 1 + 'x');
+        $(".total-count").text(parseInt($(".total-count").text()) + cost + " $");
+        return;
+    }
+    var orderItem = `
+        <div class="order-item" id = ${name} data-cost = ${cost}>
+            <img src=assets/${image} alt="">
+            <div class="item-info">
+                <h2>${name}</h2>
+                <h2><strong>1x</strong></h2>
+            </div>
+            <div class="add-container">
+                <div class="decrese">-</div>
+                <div class="count">1</div>
+                <div class="add">+</div>
+            </div>
+            <div class="delete">
+                <img src="assets/deletebtn.png" alt="">
+            </div>
+        </div>
+    `;
+    $(".total-count").text(parseInt($(".total-count").text()) + cost + " $");
+    orderContent.append(orderItem);
+});
+
+$(document).on('click', '.decrese', function () {
+    var count = parseInt($(this).parent().find('.count').text());
+    if (count == 1) {
+        $(this).parent().parent().remove();
+        $(".total-count").text(parseInt($(".total-count").text()) - parseInt($(this).parent().parent().data('cost')) + " $");
+    } else {
+        $(this).parent().find('.count').text(count - 1);
+        $(this).parent().parent().find('.item-info > h2 > strong').text(count - 1 + 'x');
+        $(".total-count").text(parseInt($(".total-count").text()) - parseInt($(this).parent().parent().data('cost')) + " $");
+    }
+});
+
+$(document).on('click', '.add', function () {
+    var count = parseInt($(this).parent().find('.count').text());
+    $(this).parent().find('.count').text(count + 1);
+    $(this).parent().parent().find('.item-info > h2 > strong').text(count + 1 + 'x');
+    $(".total-count").text(parseInt($(".total-count").text()) + parseInt($(this).parent().parent().data('cost')) + " $");
+});
+
+$(document).on('click', '.delete', function () {
+    var count = parseInt($(this).parent().find('.count').text());
+    var cost = parseInt($(this).parent().data('cost'));
+    $(".total-count").text(parseInt($(".total-count").text()) - (count * cost) + " $");
+    $(this).parent().remove();
+});
+
+$(document).on('click', '.clear-btn', function () {
+    orderContent.empty();
+    $(".total-count").text("0 $");
+});
+
+$(document).on('click', '.apply-discount', function () {
+    //var discount = parseInt($(".discount-input").val());
+    var total = parseInt($(".total-count").text());
+    $(".total-count").text((total - ((discount / 100) * total)).toFixed(0) + " $");
+});
+
+$(document).on('click', '.coppy-order-btn', function () {    
+    var order = [];
+    orderContent.children().each(function () {
+        var name = $(this).attr('id')
+        var cost = $(this).data('cost');
+        var count = $(this).find('.count').text();
+
+        //order.push(count + 'x ' + name + ' ' + cost + '$');
+        order.push(count + 'x ' + name);
+    });
+    // console.log(order);
+    // var orderString = JSON.stringify(order);
+    // console.log(orderString);
+    // navigator.clipboard.writeText(orderString);
+    var orderString = order.join(', ');
+    console.log(orderString);
+    
+    // Clipboard'a yaz
+    navigator.clipboard.writeText(orderString).then(function() {
+        console.log('Order copied to clipboard');
+    }).catch(function(err) {
+        console.error('Could not copy text: ', err);
+    });
+});
+
+$(document).on('click', '.dark-mode', function () {
+    $(".mainbody").toggleClass('dark');
+    $(".basket-container").toggleClass('dark');
+    $(".header").toggleClass('dark');
+    $(".category-item").toggleClass('dark-item');
+    $(".basket-cantainer").toggleClass('dark-order');
+    $(".content-category").toggleClass('dark-content-category');
+    $(".text").toggleClass('dark-text');
+    //$(".item-info").toggleClass('dark-text');
+    console.log('Dark mode activated');
+});
---
+++ uwucafe/config.js
@@ -0,0 +1,253 @@
+
+// Categories = {
+
+//     1: "Combos",
+//     2: "Mains",
+//     3: "Sides",
+//     4: "Deserts",
+//     5: "Drinks",
+//     6: "Business Deals"
+// }
+
+// Menus = [
+//     {
+//         "name": "Lucky Bento Box Combo",
+//         "cost": 500,
+//         "category": 1,
+//         "image": "LuckyBentoBoxCombo.png"
+//     },
+//     {
+//         "name": "9 Lives Combo",
+//         "cost": 300,
+//         "category": 1,
+//         "image": "9LivesCombo.png"
+//     },
+//     {
+//         "name": "Kitty Cat Meal",
+//         "cost": 275,
+//         "category": 1,
+//         "image": "KittyCatMeal.png"
+//     },
+//     {
+//         "name": "Cat Burglar Combo",
+//         "cost": 260,
+//         "category": 1,
+//         "image": "CatBurglarCombo.png"
+//     },
+//     {
+//         "name": "Pawcakes Combo",
+//         "cost": 185,
+//         "category": 1,
+//         "image": "PawcakesCombo.png"
+//     },
+//     {
+//         "name": "Fourth of July Meal",
+//         "cost": 175,
+//         "category": 1,
+//         "image": "FourthofJulyMeal.png"
+//     },
+//     {
+//         "name": "Bandit Combo",
+//         "cost": 150,
+//         "category": 1,
+//         "image": "BanditCombo.png"
+//     },
+//     {
+//         "name": "Om Nom Omurice",
+//         "cost": 165,
+//         "category": 2,
+//         "image": "OmNomOmurice.png"
+//     },
+//     {
+//         "name": "Sugio Cat Su Sando",
+//         "cost": 160,
+//         "category": 2,
+//         "image": "SugioCatSuSando.png"
+//     },
+//     {
+//         "name": "Kira Kira Curry",
+//         "cost": 135,
+//         "category": 2,
+//         "image": "KiraKiraCurry.png"
+//     },
+//     {
+//         "name": "Hamburg Steak",
+//         "cost": 155,
+//         "category": 2,
+//         "image": "HamburgSteak.png"
+//     },
+//     {
+//         "name": "Doki Doki Pancakes",
+//         "cost": 100,
+//         "category": 2,
+//         "image": "DokiDokiPancakes.png"
+//     },
+//     {
+//         "name": "Kitty Shish kabobs",
+//         "cost": 80,
+//         "category": 2,
+//         "image": "KittyShishkabobs.png"
+//     },
+//     {
+//         "name": "California Sushi Roll Platter",
+//         "cost": 200,
+//         "category": 2,
+//         "image": "CaliforniaSushiRollPlatter.png"
+//     },
+//     {
+//         "name": "Tempura Dragon Sushi Roll Platter",
+//         "cost": 200,
+//         "category": 2,
+//         "image": "TempuraDragonSushiRollPlatter.png"
+//     },
+//     {
+//         "name": "Spicy Girl Sushi Roll Platter",
+//         "cost": 200,
+//         "category": 2,
+//         "image": "SpicyGirlSushiRollPlatter.png"
+//     },
+//     {
+//         "name": "Chicken noodle Soup",
+//         "cost": 100,
+//         "category": 3,
+//         "image": "ChickennoodleSoup.png"
+//     },
+//     {
+//         "name": "Kitty Rice Balls",
+//         "cost": 45,
+//         "category": 3,
+//         "image": "KittyRiceBalls.png"
+//     },
+//     {
+//         "name": "Rice bowl",
+//         "cost": 40,
+//         "category": 3,
+//         "image": "Ricebowl.png"
+//     },
+//     {
+//         "name": "House Salad",
+//         "cost": 15,
+//         "category": 3,
+//         "image": "HouseSalad.png"
+//     },
+//     {
+//         "name": "Korean Corn Dogs",
+//         "cost": 100,
+//         "category": 3,
+//         "image": "KoreanCornDogs.png"
+//     },
+//     {
+//         "name": "Ballaberry Cupcake",
+//         "cost": 50,
+//         "category": 4,
+//         "image": "BallaberryCupcake.png"
+//     },
+//     {
+//         "name": "Dragon Fire Cupcake",
+//         "cost": 50,
+//         "category": 4,
+//         "image": "DragonFireCupcake.png"
+//     },
+//     {
+//         "name": "Meowchi Mochi Ice Cream",
+//         "cost": 30,
+//         "category": 4,
+//         "image": "MeowchiMochiIceCream.png"
+//     },
+//     {
+//         "name": "Ginger Kitty Cookie",
+//         "cost": 30,
+//         "category": 4,
+//         "image": "GingerKittyCookie.png"
+//     },
+//     {
+//         "name": "Purrfect Parfait",
+//         "cost": 40,
+//         "category": 4,
+//         "image": "PurrfectParfait.png"
+//     },
+//     {
+//         "name": "Oxygen Cake",
+//         "cost": 200,
+//         "category": 4,
+//         "image": "OxygenCake.png"
+//     },
+//     {
+//         "name": "Strawberry Shortcake",
+//         "cost": 200,
+//         "category": 4,
+//         "image": "StrawberryShortcake.png"
+//     },
+//     {
+//         "name": "Firework Cookies",
+//         "cost": 30,
+//         "category": 4,
+//         "image": "FireworkCookies.png"
+//     },
+//     {
+//         "name": "UwU Bomb Pop",
+//         "cost": 15,
+//         "category": 4,
+//         "image": "UwUBombPop.png"
+//     },
+//     {
+//         "name": "Boba Tea",
+//         "cost": 60,
+//         "category": 5,
+//         "image": "BobaTea.png"
+//     },
+//     {
+//         "name": "Lovely Hot Chocolate",
+//         "cost": 55,
+//         "category": 5,
+//         "image": "LovelyHotChocolate.png"
+//     },
+//     {
+//         "name": "Matcha Coffee",
+//         "cost": 55,
+//         "category": 5,
+//         "image": "MatchaCoffee.png"
+//     },
+//     {
+//         "name": "Sweet Herbal Tea",
+//         "cost": 50,
+//         "category": 5,
+//         "image": "SweetHerbalTea.png"
+//     },
+//     {
+//         "name": "Pink lemonade",
+//         "cost": 40,
+//         "category": 5,
+//         "image": "Pinklemonade.png"
+//     },
+//     {
+//         "name": "Bubble Tea",
+//         "cost": 150,
+//         "category": 5,
+//         "image": "BubbleTea.png"
+//     },
+//     {
+//         "name": "Meowcaroons 1X2",
+//         "cost": 2,
+//         "category": 6,
+//         "image": "Meowcaroons1X2.png"
+//     },
+//     {
+//         "name": "Meowcaroons 4x6",
+//         "cost": 6,
+//         "category": 6,
+//         "image": "Meowcaroons4x6.png"
+//     },
+//     {
+//         "name": "Meowcaroons 8x12",
+//         "cost": 12,
+//         "category": 6,
+//         "image": "Meowcaroons8x12.png"
+//     },
+//     {
+//         "name": "Meowcaroons 12X20",
+//         "cost": 20,
+//         "category": 6,
+//         "image": "Meowcaroons12X20.png"
+//     }
+// ]
